728ac3e98d8dc7a5a846489628c87bcc224847b9
landonain <landogriffin860@gmail.com>
2018-04-08 21:25:58 +0000

    initial loop logic

--- mario.c
+++ mario.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <cs50.h>
 
+//mario builder using user input to create size of object
 int main(void)
 {
+	while (true)
+	{
 	int i = get_int("Integer: ");
 
-	if (i <= 23 && i > 0)
-	{
-		printf("#");
+		if (i <= 23 && i > 0)
+		{
+			for (int j = 0;j <= i; j++)
+			{
+				printf("#\n");
+			}
+			//printf("%d\n", i);
+			//printf("%d\n", j);
+			//printf("  ##\n");
+			break;
+		}
 	}
-	
 }
